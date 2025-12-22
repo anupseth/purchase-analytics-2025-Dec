88cd2ebfa43d7ad494027f4cb70f9cd9569acf8b
Anupkumar Seth <anupseth11@yahoo.com>
2025-12-22 10:46:24 +0100

    Add failing test to validate CI gate

--- src/test/java/com/anup/receipt_analytics/ReceiptAnalyticsApplicationTests.java
+++ src/test/java/com/anup/receipt_analytics/ReceiptAnalyticsApplicationTests.java
@@ -3,11 +3,19 @@ package com.anup.receipt_analytics;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class ReceiptAnalyticsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldFail() {
+        fail();
 	}
 
 }
