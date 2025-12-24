0cfef225282ac8b73372064aa69d05db13b4de20
Anupkumar Seth <anupseth11@yahoo.com>
2025-12-24 10:24:42 +0100

    Run Flyway only in prod profile

---
+++ src/main/java/com/anup/receipt_analytics/config/FlywayConfig.java
@@ -0,0 +1,23 @@
+package com.anup.receipt_analytics.config;
+
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("prod")
+public class FlywayConfig {
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway(DataSource dataSource) {
+        return Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/migration")
+                .baselineOnMigrate(true)
+                .load();
+    }
+}
+
