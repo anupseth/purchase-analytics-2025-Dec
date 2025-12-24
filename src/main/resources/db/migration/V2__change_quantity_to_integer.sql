e4934c0d0f06294b5b319eeddacd03e9b4d82757
Anupkumar Seth <anupseth11@yahoo.com>
2025-12-24 19:22:50 +0100

    removing v2

--- src/main/resources/db/migration/V2__change_quantity_to_integer.sql
+++
@@ -1,6 +0,0 @@
--- Migrate numeric quantity to integer to match JPA entity
--- Safely round values when converting
-BEGIN;
-ALTER TABLE receipt_items
-    ALTER COLUMN quantity TYPE integer USING round(quantity)::integer;
-COMMIT;
